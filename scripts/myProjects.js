a4c6a2039c69cd74c00c0bb160281c8fd4b41d9f
henrybi <henrybi@u.washington.edu>
2016-01-06 06:18:43 -0800

    added seconday feature to project

---
+++ scripts/myProjects.js
@@ -0,0 +1,26 @@
+var rawData = [
+  {
+    title:       'puget sound fence',
+    category:    'web',
+    publishedOn: '2015-10-05',
+    about: 'design and built a website from scratch for a small fence builder'
+  },
+  {
+    title:       'ysmw messaging',
+    category:    'web',
+    publishedOn: '2015-09-05',
+    about: 'design and built a moving web browser ready slow motion slide show with marketing messaging'
+  },
+  {
+    title:       'cube packaging',
+    category:    'print',
+    publishedOn: '2014-5-15',
+    about: 'design a marketing package with inDesign.'
+  },
+  {
+    title:       'residential brochure',
+    category:    'graphic',
+    publishedOn: '2015-07-25',
+    about: 'created a brochure to market companys residential projects'
+  }
+]
